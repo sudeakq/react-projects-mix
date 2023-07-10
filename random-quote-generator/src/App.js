5519bb196cb46737a85f65c11a1d069b9e7a3243
sudeakq <sudeakq@gmail.com>
2023-07-10 16:54:18 +0300

    created new project

---
+++ random-quote-generator/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import {
+  useState,useEffect
+} from 'react';
+
+function App() {
+  const [quotes, setQuotes] = useState("")
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then((res)=>res.json())
+    .then((data)=>{
+      let randomNum= Math.floor(Math.random()*data.length)
+      setQuotes(data[randomNum])
+    })
+  }
+useEffect(()=>{
+  getQuote();
+},[])
+  return (
+     <div className = "App" >
+    <div className='quote'>
+    <p>{quotes.text}</p>
+    <p>{quotes.author}</p>
+    <div className="btnContainer">
+      <button onClick={getQuote} className='btn'>
+        Get Quote
+      </button>
+    </div>
+    </div>
+    </div>
+  )
+
+}
+
+export default App;
