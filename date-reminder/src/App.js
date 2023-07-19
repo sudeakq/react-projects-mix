fe5ebcfab15ec0b45618657caf3bd066deeca69a
sudeakq <sudeakq@gmail.com>
2023-07-19 11:04:07 +0300

    created new project

---
+++ date-reminder/src/App.js
@@ -0,0 +1,16 @@
+import data from './data';
+import './App.css';
+import { React, UseState } from 'react';
+console.log(data);
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <h3>NATIONAL HOLIDAY DATES</h3>
+        <button>Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ date-reminder/src/data.js
@@ -0,0 +1,27 @@
+const data = [{
+        id: 1,
+        title: "çocuk bayramı",
+        img: "https://iasbh.tmgrup.com.tr/a4abed/650/344/0/237/1600/1076?u=https://isbh.tmgrup.com.tr/sbh/2022/03/02/cocuk-sagliginda-kritik-donem-ilk-1000-gun-1646205388696.jpg",
+        date: "23 Nisan"
+    },
+    {
+        id: 2,
+        title: "çocuk bayramı2",
+        img: "https://iasbh.tmgrup.com.tr/a4abed/650/344/0/237/1600/1076?u=https://isbh.tmgrup.com.tr/sbh/2022/03/02/cocuk-sagliginda-kritik-donem-ilk-1000-gun-1646205388696.jpg",
+        date: "23 Nisan 2021"
+    },
+    {
+        id: 3,
+        title: "çocuk bayramı3",
+        img: "https://iasbh.tmgrup.com.tr/a4abed/650/344/0/237/1600/1076?u=https://isbh.tmgrup.com.tr/sbh/2022/03/02/cocuk-sagliginda-kritik-donem-ilk-1000-gun-1646205388696.jpg",
+        date: "23 Nisan 2022"
+    },
+    {
+        id: 4,
+        title: "çocuk bayramı4",
+        img: "https://iasbh.tmgrup.com.tr/a4abed/650/344/0/237/1600/1076?u=https://isbh.tmgrup.com.tr/sbh/2022/03/02/cocuk-sagliginda-kritik-donem-ilk-1000-gun-1646205388696.jpg",
+        date: "23 Nisan 2023"
+    },
+];
+
+export default data;
