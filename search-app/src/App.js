af97a1593483bbeefaab8e1568827edfd96bc51d
sudeakq <sudeakq@gmail.com>
2023-07-26 11:24:32 +0300

    created function for search

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -1,11 +1,41 @@
-import React, { useState } from "react"
-import Data from './Data.json';
+import React, { useState } from "react";
 import './App.css';
+import Data from './Data.json';
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="App">
+    <div className='container'>
+      <h1>Kurtlar Vadisi Konsey Üyeleri</h1>
+      <div className='inputContainer'>
+        <input className='search' type="text" placeholder="Konsey Üyesi Giriniz..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
 
+      </div>
+      <div className='dataContainer'>
+        {
+          Data
+            .filter((val) => {
+              if (searchTerm == "") {
+                return val;
+              } else if (val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                return val;
+              }
+            }).map((val) => {
+              return (
+                <div className="data" key={val.id}>
+
+                  <img src={val.image} alt="" />
+                  <h3>{val.title}</h3>
+                  <p>{val.Description}</p>
+
+                </div>
+              )
+            })
+        }
+      </div>
     </div>
   );
 }
