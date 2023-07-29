6f2f25f10755ea7a3155509b414b58594a9ebbe9
sudeakq <sudeakq@gmail.com>
2023-07-30 00:05:45 +0300

    added functions and basic elements

--- random-color-bg-generator/src/App.js
+++ random-color-bg-generator/src/App.js
@@ -1,18 +1,27 @@
-
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
   const [hex, setHex] = useState("#ffffff");
-  function randomHex{
-    const randomHex = "#" + Math.floor(Math.random() * 16777721).toString(16);
-    setHex(randomHex);
+
+  function randomHex() {
+    const randomhex = "#" + Math.floor(Math.random() * 16777721).toString(16);
+
+    setHex(randomhex);
   }
+
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundColor: `${hex}` }}>
       <h1>{hex}</h1>
-      <button onClick={randomHex}>Click for new background</button>
-      <button>copy the hex value</button>
+
+      <button onClick={randomHex}>click for new Background</button>
+
+      <button onClick={() => navigator.clipboard.writeText(hex)}>Copy the hex Value</button>
+
+      <h4><a href='https://github.com/fdnakn' target="_blank">Visit my Github Profile</a></h4>
+
     </div>
   );
 }
