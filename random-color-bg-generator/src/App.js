9a05b3681664cbd837a52f13e936c6eb8b570c12
sudeakq <sudeakq@gmail.com>
2023-07-29 23:58:32 +0300

    added functions

--- random-color-bg-generator/src/App.js
+++ random-color-bg-generator/src/App.js
@@ -1,7 +1,13 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [hex, setHex] = useState("#ffffff");
+  function randomHex{
+    const randomHex = "#" + Math.floor(Math.random() * 16777721).toString(16);
+    setHex(randomHex);
+  }
   return (
     <div className="App">
 
