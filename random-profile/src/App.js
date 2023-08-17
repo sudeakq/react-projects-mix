d7a6f7fd2457834ca16c603a713cfa60e6b2f3e1
sudeakq <sudeakq@gmail.com>
2023-08-17 23:18:07 +0300

    add css and axios for datas

--- random-profile/src/App.js
+++ random-profile/src/App.js
@@ -15,6 +15,12 @@ function App() {
     try {
       const res = await axios.get("https://randomuser.me/api");
       console.log(res);
+      setProfilCell(res.data.results[0].cell);
+      setProfilEmail(res.data.results[0].email);
+      setProfilImage(res.data.results[0].picture.large);
+      setProfilName(`${res.data.results[0].name.first} ${res.data.results[0].name.last}`);
+
+
 
     }
     catch (error) {
@@ -25,6 +31,23 @@ function App() {
 
   return (
     <div className="App">
+
+      <div className='profileContainer'>
+        <img src={profilImage} />
+        <h1>{profilName}</h1>
+        <p>{profilEmail}</p>
+        <p>{profilCell}</p>
+
+
+
+
+        <button onClick={ProfileData}>Click for new profile</button>
+      </div>
+
+
+
+
+
 
     </div>
   );
